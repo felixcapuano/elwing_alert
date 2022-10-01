c0089a6c87105f16d6465a2baa709375c77885d6
felixcapuano <felix.capuano@gmail.com>
2022-10-01 22:52:49 +0200

    gitignore

--- elwing_alert/main.py
+++ elwing_alert/main.py
@@ -2,10 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 
 elwing_url = "https://elwingboards.com/collections/powerkit-replacement-parts"
+webhook_discord = "https://discord.com/api/webhooks/1025867563437146173/hQNMuzkFP72paAh0k3RSDnvMzHnXTqy5HID9JzSpEzQvj-s1lRa_KaNoTcA-ovhhZDUN"
+
 product_targets = ["Moteur Powerkit V2", "Moteur Powerkit V1"]
 
-def send_notification():
-    pass
+def send_discord_notification():
+    message = "je suis content"
+    requests.post(webhook_discord, json={"content": message})
 
 def main():
     res = requests.get(elwing_url)
@@ -19,4 +22,6 @@ def main():
             sold_out = p.find("div", "grid-product__tag grid-product__tag--sold-out")
 
             print(title.string + ", sold out = " + str(sold_out != None))
+    
+    send_discord_notification()
 
